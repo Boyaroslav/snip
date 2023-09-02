01a9cf91118a04748d62dcabd5aa215dfd16ffb4
zhorik <76032531+Boyaroslav@users.noreply.github.com>
2023-09-02 18:28:42 +0300

    1.0

--- snip.c
+++ snip.c
@@ -1,13 +1,12 @@
+// Boyaroslav 2023
 
+//thanks to https://stackoverflow.com/questions/8249669/how-do-take-a-screenshot-correctly-with-xlib
 #include<stdio.h>
 #include<stdlib.h>
 #include<X11/Xlib.h>
 #include<X11/cursorfont.h>
-#include<unistd.h> // added for sleep/usleep
+#include<unistd.h>
 #include<png.h>
-// original from [https://bbs.archlinux.org/viewtopic.php?id=85378 Select a screen area with mouse and return the geometry of this area? / Programming & Scripting / Arch Linux Forums]
-// build with (Ubuntu 14.04):
-// gcc -Wall xrectsel.c -o xrectsel -lX11
 
 int main(int argc, char **argv)
 {
